956e6769652d64aaceed0c0079eecd0ff175d161
NickAzz <nick.azurin@gmail.com>
2021-05-25 21:51:09 +0000

    all 4_queries files

--- 4_queries/6_largest_time.sql
+++ 4_queries/6_largest_time.sql
@@ -2,4 +2,5 @@ SELECT cohorts.name, SUM(completed_at - started_at)/COUNT(*) AS average_assistan
 FROM assistance_requests JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 GROUP BY cohorts.name
-ORDER BY average_assistance_time;
+ORDER BY average_assistance_time DESC
+LIMIT 1;
