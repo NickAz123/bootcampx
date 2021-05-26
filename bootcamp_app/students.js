96676958189121c5a6eac247b91be195905a8307
NickAzz <nick.azurin@gmail.com>
2021-05-26 23:08:06 +0000

    added bootcamp APP js

---
+++ bootcamp_app/students.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name
+FROM students JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3]};`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.student_name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+  })
+})
+.catch(err => console.error('Query Error', err.stack));
